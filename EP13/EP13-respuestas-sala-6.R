828d389b6d5f7117c119f059b7f3e32318d7eb0f
Claudio <claudio.munoz.m@usach.cl>
2022-01-15 16:01:34 -0300

    Se termina el EP13

--- EP13/EP13-respuestas-sala-6.R
+++ EP13/EP13-respuestas-sala-6.R
@@ -35,7 +35,6 @@ if (!require(pROC)){
   require (pROC)
 }
 
-
 # Se cargan de datos.
 datos <- read.csv2(file.choose(),header=TRUE)
 datos_num <- as.data.frame(apply(datos, 2, as.numeric))
@@ -116,14 +115,12 @@ cat("Evaluación del modelo completo basada en validación cruzada :\n")
 matriz.modelo.completo <- confusionMatrix(modelo.completo$pred$pred, modelo.completo$pred$obs)
 print(matriz.modelo.completo)
 
-# No soporta los modelos ajustados con Train...
+# Para elegir las variables predictoras se utilizará la función step(), sin embargo 
+# esta no soporta los modelos ajustados con Train
 # modelo.escalonado <- step(modelo, scope = list(lower = modelo, upper = modelo.completo), direction = "both", trace = 0)
 
-
-
-
 #### Modelos usando gml
-
+set.seed(1111)
 # Separar conjuntos de entrenamiento y prueba .
 n <- nrow(datos_num)
 n_entrenamiento <- floor(0.8 * n)
@@ -141,19 +138,21 @@ print(summary(complete_model))
 
 stepped_model <- step(simple_model, scope = list(lower = simple_model, upper = complete_model), direction = "both", trace = 0)
 print(summary(stepped_model))
+cat ( " AIC =" , AIC ( stepped_model ) , "\n\n" )
 
 # Obtenemos que el modelo propuesto añade los siguientes 6 predictores:
 # 
-# Height            -0.42537    0.06108  -6.964 3.30e-12 ***
-# Knee.Girth         0.78735    0.15616   5.042 4.61e-07 ***
-# Forearm.Girth      0.74060    0.17768   4.168 3.07e-05 ***
-# Biiliac.diameter   0.40354    0.13320   3.030 0.002448 ** 
-# Chest.depth        0.32791    0.15845   2.070 0.038497 *  
-# Wrists.diameter    0.69036    0.42171   1.637 0.101614
+#  Waist.Girth        0.27307    0.04709   5.799 6.66e-09 ***
+#  Height            -0.39042    0.04972  -7.853 4.06e-15 ***
+#  Forearm.Girth      0.83796    0.14374   5.830 5.55e-09 ***
+#  Chest.depth        0.21687    0.13471   1.610 0.107407    
+#  Knee.Girth         0.61150    0.12939   4.726 2.29e-06 ***
+#  Biiliac.diameter   0.38541    0.11917   3.234 0.001221 ** 
 
-# Como se nos pide añadir entre dos y cinco predictores, eliminaremos Wrists.diameter
-# pues es la variable menos significativa de todas.
-# 
+# Como se nos pide añadir entre dos y cinco predictores se decide tomar las 5 variables, 
+# obviando la variable Waist.Grith ya que esta se encuentra incluida desde el punto 2
+# donde se nos pide elegir una variable predictora.
+
 # Quedandonos el siguiente modelo
 
 predictoras.final <- c("Height", "Forearm.Girth", "Chest.depth", "Knee.Girth", "Biiliac.diameter")
@@ -166,6 +165,8 @@ modelo.final <- train(f.final, data = datos_num, method = "glm",
 )
 print(summary(modelo.final))
 
+
+
 # Evaluar el modelo final
 cat("Evaluación del modelo final basada en validación cruzada :\n")
 matriz.modelo.final <- confusionMatrix(modelo.final$pred$pred, modelo.final$pred$obs)
@@ -177,29 +178,68 @@ print(matriz.modelo.final)
 # Evaluar los modelos y “arreglarlos” en caso de que tengan algún problema con las condiciones que deben
 # cumplir.
 
+modelo.final.glm <- glm(f.final, family = binomial(link = "logit"), data = datos_num)
+
+# Independencia de los residuos .
+cat("Verificación de independencia de los residuos\n")
+cat("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\ n " )
+print(durbinWatsonTest(modelo.final.glm, max.lag = 5))
+
+# Podemos concluir que los residuos son, en efecto, independientes.
+
+# Se evalua si cada predictor se relaciona linealmente con la variable de respuesta.
+
+datos.PR <- select(datos_num, Waist.Girth, Height, Forearm.Girth, Chest.depth, Knee.Girth, Biiliac.diameter, EN)
+datos.PR <- apply(datos.PR, 2, as.numeric)
+correlacion <- round(cor(datos.PR), 2)
+correlacion
+corrplot(correlacion, method="number", type="upper")
+
+# Como se puede observar se verifica que existe una relación lineal con los predictores y 
+# la variable de respuesta.
+
+# Verificación de multicolinealidad.
+cat("Verificación de colinealidad\n")
+cat("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n")
+cat("\nVIF:\n")
+vifs <- vif(modelo.final.glm)
+print(vifs)
+cat ("- Tolerancias :\n")
+print (1 / vifs )
+cat("\nPromedio VIF:")
+print(mean(vifs))
+
+# Como umbral de preocupación se toma un valor de 10, como se pudo verificar 
+# ninguna variable predictora superó el umbral, sin embargo, puede verse afectada 
+# por cierta colinealidad dado que hay valores que son mayores a 5. además el VIF 
+# promedio es mayor a 1, por lo que podría haber sesgo en el modelo. Por esta razón
+# se considera eliminar la variable Height para mejorar el modelo y obtener uno mas
+# confiable.
 
 
+predictoras.mejorado <- c("Forearm.Girth", "Chest.depth", "Knee.Girth", "Biiliac.diameter")
+f.mejorado <- as.formula(paste("EN", paste(c(predictor, predictoras.mejorado), collapse = "+"), sep = " ~ "))
+modelo.mejorado <- train(f.mejorado, data = datos_num, method = "glm",
+                         family = binomial(link = "logit"),
+                         trControl = trainControl(method = "cv", number = 5, savePredictions = TRUE))
 
 # Evaluar el poder predictivo de los modelos en datos no utilizados para construirlo (o utilizando validación 
 # cruzada) y revisar las respectivas curvas ROC.
 
+# Separar conjuntos de entrenamiento y prueba .
+n <- nrow ( datos )
+n_entrenamiento <- floor(0.8 * n )
+muestra <- sample.int( n = n , size = n_entrenamiento , replace = FALSE )
+entrenamiento <- datos[ muestra, ]
+prueba <- datos[- muestra , ]
 
 # Evaluar el modelo
-cat("Evaluación del modelo basada en validación cruzada :\n")
+cat("Evaluación del modelo basada en validación cruzada:\n")
 matriz <- confusionMatrix(modelo$pred$pred, modelo$pred$obs)
 print(matriz)
 
-roc.modelo <- roc(datos_num[["EN"]], as.numeric(modelo$pred$pred))
-plot(roc.modelo)
-
-
-# Evaluar el modelo completo
-cat("Evaluación del modelo completo basada en validación cruzada :\n")
-matriz.modelo.completo <- confusionMatrix(modelo.completo$pred$pred, modelo.completo$pred$obs)
-print(matriz.modelo.completo)
-
-roc.modelo.completo <- roc(datos_num[["EN"]], as.numeric(modelo.completo$pred$pred))
-plot(roc.modelo.completo)
+roc.modelo <- roc(modelo$pred$obs, as.numeric(modelo$pred$pred))
+plot(roc.modelo, main = "Modelo Variable única")
 
 
 # Evaluar el modelo final
@@ -207,9 +247,20 @@ cat("Evaluación del modelo final basada en validación cruzada :\n")
 matriz.modelo.final <- confusionMatrix(modelo.final$pred$pred, modelo.final$pred$obs)
 print(matriz.modelo.final)
 
-roc.modelo.final <- roc(datos_num[["EN"]], as.numeric(modelo.final$pred$pred))
-plot(roc.modelo.final)
+roc.modelo.final <- roc(as.numeric(modelo.final$pred$obs), as.numeric(modelo.final$pred$pred))
+plot(roc.modelo.final, main = "Modelo con todos los predictores")
 
+# Evaluar el modelo mejorado
+cat("Evaluación del modelo mejorado basada en validación cruzada :\n")
+matriz.modelo.mejorado <- confusionMatrix(modelo.mejorado$pred$pred, modelo.mejorado$pred$obs)
+print(matriz.modelo.mejorado)
 
+roc.modelo.mejorado <- roc(modelo.mejorado$pred$obs, as.numeric(modelo.mejorado$pred$pred))
+plot(roc.modelo.mejorado, main = "Modelo Mejorado")
+
+# En relación a los datos y los gráficos que se pueden observar los mejores modelos son
+# el modelo con las 5 variables predictoras junto con el modelo mejorado, sin embargo
+# como se mencionó anteriormente el modelo con las 5 variables predictoras es posible que presente 
+# sesgo por lo que se considera el modelo mejorado como modelo más preciso.
 
 
